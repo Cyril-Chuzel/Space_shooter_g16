eb044e3c55bf4cf2b5927355019b13fc5a73262b
Cyril-Chuzel <60505014+Cyril-Chuzel@users.noreply.github.com>
2020-04-21 11:36:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -8,22 +8,22 @@ window.addEventListener('keydown', (event) =>
     if(event.code == 'ArrowUp')
     {
         playerTop -= playerSpeed
-        player.style.transform = `rotate(90deg)`
+        player.style.transform = `rotate(0deg)`
     }
     else if(event.code == 'ArrowRight')
     {
         playerLeft += playerSpeed
-        player.style.transform = `rotate(180deg)`
+        player.style.transform = `rotate(90deg)`
     }
     else if(event.code == 'ArrowDown')
     {
         playerTop += playerSpeed
-        player.style.transform = `rotate(-90deg)`
+        player.style.transform = `rotate(180deg)`
     }
     else if(event.code == 'ArrowLeft')
     {
         playerLeft -= playerSpeed
-        player.style.transform = `rotate(0deg)`
+        player.style.transform = `rotate(-90deg)`
     }
     player.style.top = `${playerTop}px`
     player.style.left = `${playerLeft}px`
