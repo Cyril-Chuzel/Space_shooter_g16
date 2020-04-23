a57f868b56e30a935a04fad421df7b8c854bbc23
Cyril-Chuzel <60505014+Cyril-Chuzel@users.noreply.github.com>
2020-04-24 00:05:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
 const nebula = document.querySelector('.nebula')
 const stars = document.querySelector('.stars')
+
 const scoreBoard = document.querySelector('.scoreBoard')
 const timer = document.querySelector('.time')
+
 const pause = document.querySelector('.pause')
 const player = document.querySelector('.player')
+
 const pauseScreen = document.querySelector('.pauseScreen')
 const pauseContainer = pauseScreen.querySelector('.pauseContainer')
 const resume = pauseContainer.querySelector('.resume')
 const menu = pauseContainer.querySelector('.menu')
+
 const changeShip = pauseContainer.querySelector('.changeShip')
 const shipR = changeShip.querySelector('.R')
 const shipB = changeShip.querySelector('.B')
 const shipG = changeShip.querySelector('.G')
+
+const ennemies = document.querySelector('.enemies')
+const ennemi1 = ennemies.querySelector('.first')
+const ennemi2 = ennemies.querySelector('.second')
+const ennemi3 = ennemies.querySelector('.third')
+const ennemi4 = ennemies.querySelector('.fourth')
+const ennemi5 = ennemies.querySelector('.fifth')
+const ennemi6 = ennemies.querySelector('.sixth')
+const ennemi7 = ennemies.querySelector('.seventh')
+const ennemi8 = ennemies.querySelector('.eigth')
+
 let backgroundScroll = 0
 let score = 0
 let time = 0
 let paused = false
+let random = 0
+
+let ennemiP1 = 0
+let ennemiP2 = 0
+let ennemiP3 = 0
+let ennemiP4 = 0
+let ennemiP5 = 0
+let ennemiP6 = 0
+let ennemiP7 = 0
+let ennemiP8 = 0
+
+const ennemiesSpawn = [ennemi1, ennemi2, ennemi3, ennemi4, ennemi5, ennemi6, ennemi7, ennemi8]
+const ennemiesList = ['./sprites/vaisseaux/enemis/ennemi-1.png' , './sprites/vaisseaux/enemis/ennemi-2.png' , './sprites/vaisseaux/enemis/ennemi-3.png' , './sprites/vaisseaux/enemis/ennemi-4.png' , './sprites/vaisseaux/enemis/ennemi-5.png' , './sprites/vaisseaux/enemis/ennemi-6.png' , './sprites/vaisseaux/enemis/ennemi-7.png' , './sprites/vaisseaux/enemis/ennemi-8.png']
+const ennemiesPosition = [ennemiP1, ennemiP2, ennemiP3, ennemiP4, ennemiP5, ennemiP6, ennemiP7, ennemiP8]
 
 {
     //==Varriable dans le scope==
@@ -110,15 +139,44 @@ resume.addEventListener('click', () =>
     paused = false
 })
 
-//====== Scrolling of backgrounds images and time counter
+//====== Time based operations
 window.setInterval(function()
 {
-if (paused == false)
+    if (paused == false)
     {
+        // scrolling of the backgrounds and time counter
         backgroundScroll += 20
-        time += 1
+        time ++
         nebula.style.paddingTop = `${backgroundScroll}px`
         stars.style.paddingTop = `${backgroundScroll*3}px`
         timer.value = `${time}s  `
+        //randomised ennemies spawning system
+        if (time%5 == 0)
+        {
+            random = (Math.floor(Math.random()*9))
+            if (random != 0)
+            {
+                for (let i = 0; i < random ; i ++)
+                {
+                    let j = (Math.floor(Math.random()*(random-1)))
+                    ennemiesSpawn[j].setAttribute('src', ennemiesList[(Math.floor(Math.random()*8))])
+                    ennemiesSpawn[j].style.visibility = `visible`
+                }
+            }
+        }
+        for (k = 0; k < 7; k ++)
+        {
+            if (ennemiesSpawn[k].style.visibility == `visible`)
+            {
+                ennemiesPosition[k] += 4
+                ennemiesSpawn[k].style.marginTop = `${ennemiesPosition[k]}vh`
+                if (ennemiesPosition[k] > 90)
+                {
+                    ennemiesSpawn[k].style.visibility = `hidden`
+                    ennemiesPosition[k] = 0
+                    ennemiesSpawn[k].style.marginTop = `${ennemiesPosition[k]}vh`
+                }
+            }
+        }
     }
 }, 1000)
